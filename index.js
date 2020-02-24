038fb4b9c05dfc94a1bf96e8ac4fb7396f689794
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-02-24 14:03:32 -0500

    Going to bounce into login/register

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ const logger = require('morgan');
 // require cors
 const cors = require('cors');
 // set port variable
-
 const port = 8000 || process.env.port ;
 
 // initialize server, add middle-ware
+const server = express();
+
+server.use(helmet(),
+           cors(), 
+           express.json(),
+           logger('dev')
+           );
+
+server.get();
+
+server.post();
+
+server.delete();
+
+server.put();
