9f17a05b3972adddbb2b82cb46fec0f04a9a5616
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-04-12 15:49:11 -0700

    Implementing NuID, working through header errors.

--- API/Routes/login-route.js
+++ API/Routes/login-route.js
@@ -1,10 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config()
 // require express
 const express = require('express');
 // require router
 // require axios
 const axios = require('axios');
 // require jwt
-const jwt = require('jwt');
 // require bcrypt
 const bcrypt = require('bcryptjs');
 // require pw_check
@@ -13,36 +14,27 @@ const pw_check = require('../MW-Functions/middleware');
 const userDB = require('../DB-Functions/user-functions');
 // generate token
 
+
 const Zk = require('@nuid/zk');
 
-const body = Zk.proofFromSecret('test');
+const base_url = "https://auth.nuid.io/";
 
-const base_url = "https://auth.nuid.io/"
 
-const generate_token = function(user) {
-  const payload = {user: user};
-  const secret = process.env.JWT_SEC;
-  const options = {
-    expiresIn: "8h",
-    jwtid: "e6^$@#AHD*@#D9230gr@J12R2@*^#t"
-  },
-
-  return jwt.sign(payload, secret, options);
-}
-
-const makeConfig = (method, body, headers) => {
-  if (headers) {
-    const axiosConfigObject = {method: method, body: JSON.stringify(body), headers: headers}
-  }
-  else {
-    
-    const axiosConfigObject = {method: method, body: JSON.stringify(body)}
-  }
+const makeConfig = (body) => {
+    const axiosConfigObject = {body: JSON.stringify(body), headers: {'X-API-KEY': process.env.NUID_API_KEY, 'Content-Type': 'application/json'}};
+    return axiosConfigObject;
 }
 
 const generateCredential = async () => {
-  axios.post(base_url, makeConfig('POST', body,))
+  const proof = Zk.proofFromSecret('test');
+  try {
+    const response = await axios.post(base_url, makeConfig(proof));
+    console.log(response);
+  }
+  catch (error) {
+    let errorObj = {error: error.message, error_msg: "There was an error generating credentials."}
+    console.log(error)
+  }
 }
 
-
-module.exports = router;
+console.log(generateCredential())
